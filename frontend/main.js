b98dc3398a785026bc6e59f0c84fe01ea408e3cc
elimei1 <112986140+elimei1@users.noreply.github.com>
2024-11-12 23:40:28 +0100

    fetch api call, group images, maxDistance, reload

--- frontend/main.js
+++ frontend/main.js
@@ -1,86 +1,123 @@
 function startExperience() {
     document.getElementById('welcomeScreen').style.display = 'none';
     document.getElementById('aframeScene').style.display = 'block';
-    loadImages();
+    loadImages().catch(error => console.error('Error in startExperience:', error));
+    setInterval(updateUserPosition, 10000); // update user position every 10 seconds
 }
 
-const lat = 48.158145
-const lon = 16.381274
+let imagesArray = [];
+const MAX_DISTANCE = 30;
+let firstUpdate = true;
 
-const imagesArray = [
-    {
-        src: "https://as2.ftcdn.net/v2/jpg/00/71/38/29/1000_F_71382959_eYupFs1L5m21OYLXope4K9HXar7HQKsv.jpg",
-        caption: "A beautiful day here!",
-        latitude: lat,
-        longitude: lon
-    },
-    {
-        src: "https://as2.ftcdn.net/v2/jpg/01/83/98/81/1000_F_183988193_ihuHiHc4ZFiYqL3H4qZkDRAAb9HfGVHp.jpg",
-        caption: "Hello from here!",
-        latitude: lat,
-        longitude: lon
-    },
-    {
-        src: "https://as2.ftcdn.net/v2/jpg/00/49/14/31/1000_F_49143198_5HdyJHUqgkAkbOELEsre7oH1c9MwfOQb.jpg",
-        caption: "What a nice view!",
-        latitude: lat,
-        longitude: lon
-    },
-    {
-        src: "https://as2.ftcdn.net/v2/jpg/00/70/88/07/1000_F_70880772_7JFkehQCDV81ALUhic4CpCrCr8skuy0s.jpg",
-        caption: "Whats up!",
-        latitude: lat,
-        longitude: lon
-    },
-    {
-        src: "https://as2.ftcdn.net/v2/jpg/00/48/88/49/1000_F_48884911_sD1WzCWgPTHmlJ9Lsgiw3OnV6FrmZ88I.jpg",
-        caption: "I'm soo cool!",
-        latitude: lat,
-        longitude: lon
-    },
-    {
-        src: "https://as1.ftcdn.net/v2/jpg/04/21/00/54/1000_F_421005472_GJR2bkRLvY1uupdBIyKGZknTWsl0dWik.jpg",
-        caption: "This is crazy!",
-        latitude: lat,
-        longitude: lon
-    },
+async function loadImages() {
+    try {
+        // fetch images from API
+        const response = await fetch('https://run.mocky.io/v3/06220c5d-c598-4ba4-b0c6-047680b23ac8');
+        const data = await response.json();
+        imagesArray = data.images;
 
-];
+        updateUserPosition();
+    } catch (error) {
+        console.error('Error loading images:', error);
+    }
+}
 
-function loadImages() {
+function updateUserPosition() {
     const scene = document.querySelector("a-scene");
-    const radius = 20
-
-    imagesArray.forEach((imageData, index) => {
-        const imageEntity = document.createElement("a-entity");
-        imageEntity.setAttribute("gps-new-entity-place", {
-            latitude: imageData.latitude,
-            longitude: imageData.longitude
+    if (!firstUpdate) {
+        // clear all groups
+        Array.from(scene.querySelectorAll("[gps-new-entity-place]")).forEach(child => {
+            scene.removeChild(child);
         });
+    } else {
+        firstUpdate = false;
+    }
 
+    // get user location
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const userLatitude = position.coords.latitude;
+            const userLongitude = position.coords.longitude;
+
+            // group images based on coordinates
+            const groupedImages = groupImagesByLatLong(imagesArray);
+
+            // iterate over each group
+            for (const [latLongKey, imagesGroup] of Object.entries(groupedImages)) {
+                const [latitude, longitude] = latLongKey.split(',').map(Number);
+                const distance = calculateDistance(userLatitude, userLongitude, latitude, longitude);
+
+                // only add if close to user
+                if (distance <= MAX_DISTANCE) {
+                    const groupEntity = createGroupEntity(latitude, longitude, imagesGroup);
+                    scene.appendChild(groupEntity);
+                }
+            }
+        }, (error) => {
+            console.error('Error getting user location:', error);
+        });
+    } else {
+        console.error('Geolocation is not supported by this browser.');
+    }
+}
+
+function groupImagesByLatLong(images) {
+    // group by 4 digits after the comma
+    return images.reduce((grouped, image) => {
+        const key = `${image.latitude.toFixed(4)},${image.longitude.toFixed(4)}`;
+        grouped[key] = grouped[key] || [];
+        // push to group based on key
+        grouped[key].push(image);
+        return grouped;
+    }, {});
+}
+
+function createGroupEntity(latitude, longitude, imagesGroup) {
+    const groupEntity = document.createElement("a-entity");
+    groupEntity.setAttribute("gps-new-entity-place", { latitude, longitude });
+
+    // add each image to group
+    imagesGroup.forEach((imageData, index) => {
         const img = document.createElement("a-image");
         img.setAttribute("src", imageData.src);
         img.setAttribute("width", "7.5");
         img.setAttribute("height", "11");
         img.setAttribute("look-at", "[gps-new-camera]");
+        // offset to avoid overlapping
+        img.object3D.position.set((index % 2 === 0 ? -1 : 1) * (index + 1) * 4, 0, 0);
 
-        const angle = (2 * Math.PI * index) / 10;
-        const x = radius * Math.cos(angle);
-        const z = radius * Math.sin(angle);
-        img.object3D.position.set(x, 0, z);
-
-        imageEntity.appendChild(img);
-
-        const text = document.createElement("a-text");
-        text.setAttribute("value", imageData.caption);
-        text.setAttribute("position", `0 -7 0`);
-        text.setAttribute("color", "#FFF");
-        text.setAttribute("align", "center");
-        text.setAttribute("scale", "4 4 2");
-        text.setAttribute("look-at", "[gps-new-camera]");
+        // caption text
+        const text = createCaptionText(imageData.caption);
         img.appendChild(text);
-
-        scene.appendChild(imageEntity);
+        groupEntity.appendChild(img);
     });
 
+    return groupEntity;
+}
+
+function createCaptionText(caption) {
+    const text = document.createElement("a-text");
+    text.setAttribute("value", caption);
+    text.setAttribute("position", "0 -7 0");
+    text.setAttribute("color", "#FFF");
+    text.setAttribute("align", "center");
+    text.setAttribute("scale", "4 4 2");
+    text.setAttribute("look-at", "[gps-new-camera]");
+    return text;
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    // distance with Haversine formula
+    const R = 6371e3; // radius of Earth
+    const φ1 = lat1 * Math.PI / 180;
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat1) * Math.PI / 180;
+    const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
 }
