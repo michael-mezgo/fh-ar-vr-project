d18f2d45e43362f36ed0a6c3df18fc638a8b3bed
elimei1 <112986140+elimei1@users.noreply.github.com>
2024-11-18 16:24:40 +0100

    8080

--- frontend/upload.js
+++ frontend/upload.js
@@ -93,7 +93,7 @@ uploadButton.addEventListener("click", async () => {
             latitude: currentLat,
             picture: base64String
         }
-        const response = await fetch("https://localhost:8082/api/uploadPicture", {
+        const response = await fetch("https://localhost:8080/api/uploadPicture", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
